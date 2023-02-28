f416827651bf131f5acb67c704917e5dafc0ff7e
Davronbek Sharipov <davronking001@gmail.com>
2023-02-28 10:52:39 +0500

    Completed string task

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,11 +9,36 @@ public class StringSplitter {
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        boolean begin = false;
+        String temp = "";
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < source.length(); i++) {
+            char ch = source.charAt(i);
+            String stringCH = String.valueOf(ch);
+            for (String delimiter : delimiters) {
+                if (delimiter.equals(stringCH)) {
+                    begin = true;
+                    break;
+                }
+            }
+            if (begin) {
+                if (!temp.isEmpty()) {
+                    res.add(temp);
+                    temp = "";
+                }
+                begin = false;
+            } else {
+                temp += stringCH;
+            }
+        }
+        if (!temp.isEmpty()) {
+            res.add(temp);
+        }
+        return res;
     }
 }
